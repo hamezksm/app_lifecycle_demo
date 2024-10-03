badfcd14db3b1688e56bf9e66936268b70fd9d44
hamezksm <jamcav097@gmail.com>
2024-10-03 22:11:37 +0300

    First commit. This is a demo of how an app lifecycle behaves. From resumed to inactive to hidden to paused to restarted to onshow to onresume. Really interesting insights honestly.

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:app_lifecycle_demo/app_lifecycle_display.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(body: AppLifecycleDisplay()),
+    );
+  }
+}
